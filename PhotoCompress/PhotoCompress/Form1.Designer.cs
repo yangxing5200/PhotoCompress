0fd9245a70fc772b1748b8321b43f52ee9996434
jason <y.jason@cjatech.com>
2015-06-01 14:17:51 +0800

    日志加时间

--- PhotoCompress/PhotoCompress/Form1.Designer.cs
+++ PhotoCompress/PhotoCompress/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.chkRotate = new System.Windows.Forms.CheckBox();
             this.btn_Begin = new System.Windows.Forms.Button();
@@ -254,6 +255,7 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMain";
